215bd0140e56f653bb4cc0c879c7b18298834ba8
Rahul Chaudhari <chaudhariraul5@gmail.com>
2025-05-31 23:14:21 +0545

    set up folders and organize for Sprint-2

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quickstock/view/signup_page.dart';
 import 'package:quickstock/view/splash_screen.dart';
 
 class MyApp extends StatelessWidget {
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quickstock/app.dart';
 
-void main() {
+void main() { 
   runApp(const MyApp());
 }
