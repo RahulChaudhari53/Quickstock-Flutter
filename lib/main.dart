42aca6fdc53face192a445debb66b10ca67ad5df
Rahul Chaudhari <chaudhariraul5@gmail.com>
2025-06-01 00:50:32 +0545

    Set system navigation bar color and icon brightness in main()

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:quickstock/app.dart';
 
-void main() { 
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ),
+  );
+
   runApp(const MyApp());
 }
