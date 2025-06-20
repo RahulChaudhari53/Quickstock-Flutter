4b18d9070ed0d3feb92dff9213e92715e6ed2c3a
Rahul Chaudhari <chaudhariraul5@gmail.com>
2025-06-20 06:48:32 +0545

    implement service locator for dependency injection

---
+++ lib/app/service_locator/service_locator.dart
@@ -0,0 +1,67 @@
+import 'package:get_it/get_it.dart';
+import 'package:quickstock/core/network/hive_service.dart';
+import 'package:quickstock/features/dashboard/presentation/view_model/dashboard_view_model.dart';
+import 'package:quickstock/features/splash/presentation/view_model/splash_view_model.dart';
+import 'package:quickstock/features/user/data/data_source/local_data_source/user_local_data_source.dart';
+import 'package:quickstock/features/user/data/repository/local_repository/user_local_repository.dart';
+import 'package:quickstock/features/user/domain/usecase/user_login_usecase.dart';
+import 'package:quickstock/features/user/domain/usecase/user_register_usecase.dart';
+import 'package:quickstock/features/user/presentation/view_model/login_view_model/login_view_model.dart';
+import 'package:quickstock/features/user/presentation/view_model/register_view_model/register_view_model.dart';
+
+final serviceLocator = GetIt.instance;
+
+Future<void> initDependencies() async {
+  await _initHiveService();
+
+  await _initSplashModule();
+  await _initUserModule();
+  await _initDashboardModule();
+}
+
+Future<void> _initHiveService() async {
+  serviceLocator.registerLazySingleton(() => HiveService());
+}
+
+Future<void> _initUserModule() async {
+  // ============== Data Source ==============
+  serviceLocator.registerFactory(
+    () => UserLocalDataSource(hiveService: serviceLocator<HiveService>()),
+  );
+
+  // ============== Repository ==============
+  serviceLocator.registerFactory(
+    () => UserLocalRepository(
+      userLocalDataSource: serviceLocator<UserLocalDataSource>(),
+    ),
+  );
+
+  // ============== Usecase ==============
+  serviceLocator.registerFactory(
+    () => UserRegisterUsecase(
+      iUserRepository: serviceLocator<UserLocalRepository>(),
+    ),
+  );
+
+  serviceLocator.registerFactory(
+    () => UserLoginUsecase(
+      iUserRepository: serviceLocator<UserLocalRepository>(),
+    ),
+  );
+
+  serviceLocator.registerFactory(
+    () => RegisterViewModel(serviceLocator<UserRegisterUsecase>()),
+  );
+
+  serviceLocator.registerFactory(
+    () => LoginViewModel(serviceLocator<UserLoginUsecase>()),
+  );
+}
+
+Future<void> _initDashboardModule() async {
+  serviceLocator.registerFactory(() => DashboardViewModel());
+}
+
+Future<void> _initSplashModule() async {
+  serviceLocator.registerFactory(() => SplashViewModel());
+}
