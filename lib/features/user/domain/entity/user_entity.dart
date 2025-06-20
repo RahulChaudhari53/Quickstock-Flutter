7afcb3a6f2b9613a2b58b3b77b41caeb782f4bce
Rahul Chaudhari <chaudhariraul5@gmail.com>
2025-06-20 06:57:05 +0545

    add user domain layer entity

---
+++ lib/features/user/domain/entity/user_entity.dart
@@ -0,0 +1,41 @@
+import 'package:equatable/equatable.dart';
+
+class UserEntity extends Equatable {
+  final String? userId;
+  final String firstName;
+  final String lastName;
+  final String email;
+  final List<String> phoneNumbers;
+  final String? profileImage;
+  final String role;
+  final String password;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  const UserEntity({
+    this.userId,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.phoneNumbers,
+    this.profileImage,
+    required this.role,
+    required this.password,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  @override
+  List<Object?> get props => [
+    userId,
+    firstName,
+    lastName,
+    email,
+    phoneNumbers,
+    profileImage,
+    role,
+    password,
+    createdAt,
+    updatedAt,
+  ];
+}
