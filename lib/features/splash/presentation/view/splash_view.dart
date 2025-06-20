d99696a05d63c01fdd48560ad2dcf62760c6d4e0
Rahul Chaudhari <chaudhariraul5@gmail.com>
2025-06-20 06:52:43 +0545

    prepare dashboard for clean architecture

---
+++ lib/features/splash/presentation/view/splash_view.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quickstock/app_config.dart';
+import 'package:quickstock/features/splash/presentation/view_model/splash_view_model.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<SplashViewModel>().init(context);
+    });
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/logo/quickstock_logo.png',
+              width: 150,
+              height: 150,
+            ),
+            const SizedBox(height: 20),
+            // const Text(
+            //   AppConfig.appName,
+            //   style: TextStyle(
+            //     fontSize: 24,
+            //     fontWeight: FontWeight.bold,
+            //     color: Colors.black,
+            //   ),
+            // ),
+            const SizedBox(height: 20),
+            const CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/splash/presentation/view_model/splash_view_model.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quickstock/app/service_locator/service_locator.dart';
+import 'dart:async';
+import 'package:quickstock/features/user/presentation/view/login_view.dart';
+import 'package:quickstock/features/user/presentation/view_model/login_view_model/login_view_model.dart';
+
+class SplashViewModel extends Cubit<void> {
+  SplashViewModel() : super(null);
+
+  Future<void> init(BuildContext context) async {
+    await Future.delayed(const Duration(seconds: 2), () {
+      if (context.mounted) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder:
+                (context) => BlocProvider.value(
+                  value: serviceLocator<LoginViewModel>(),
+                  child: LoginView(),
+                ),
+          ),
+        );
+      }
+    });
+  }
+}
