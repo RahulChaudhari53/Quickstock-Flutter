ef836c7435327f44561543f606effa6c08818b10
Rahul Chaudhari <chaudhariraul5@gmail.com>
2025-07-10 11:39:12 +0545

    apply saveToken during logiin

--- lib/features/user/domain/usecase/user_login_usecase.dart
+++ lib/features/user/domain/usecase/user_login_usecase.dart
@@ -1,5 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:quickstock/app/shared_pref/token_shared_pref.dart';
 import 'package:quickstock/app/usecase/usecase.dart';
 import 'package:quickstock/core/error/failure.dart';
 import 'package:quickstock/features/user/domain/repository/user_repository.dart';
@@ -17,15 +19,29 @@ class LoginUserParams extends Equatable {
 
 class UserLoginUsecase implements UsecaseWithParams<String, LoginUserParams> {
   final IUserRepository _iUserRepository;
+  final TokenSharedPref _tokenSharedPref;
 
-  const UserLoginUsecase({required IUserRepository iUserRepository})
-    : _iUserRepository = iUserRepository;
+  const UserLoginUsecase({
+    required IUserRepository iUserRepository,
+    required TokenSharedPref tokenSharedPref,
+  }) : _iUserRepository = iUserRepository,
+       _tokenSharedPref = tokenSharedPref;
 
   @override
   Future<Either<Failure, String>> call(LoginUserParams params) async {
-    return await _iUserRepository.loginUser(
+    final token = await _iUserRepository.loginUser(
       params.phoneNumber,
       params.password,
     );
+
+    return token.fold((failure) => Left(failure), (token) {
+      _tokenSharedPref.saveToken(token).then((result) {
+        result.fold(
+          (failure) => debugPrint("Failed to save token: ${failure.message}"),
+          (_) => debugPrint("Saved Token Successfully."),
+        );
+      });
+      return Right(token);
+    });
   }
 }
