4038d1af4ba42a4f082361edd6ce6af5ace53a24
Rahul Chaudhari <chaudhariraul5@gmail.com>
2025-07-11 18:18:33 +0545

    remove default values from controllers

--- lib/features/user/presentation/view/register_view.dart
+++ lib/features/user/presentation/view/register_view.dart
@@ -12,21 +12,28 @@ class RegisterView extends StatelessWidget {
   RegisterView({super.key});
 
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-  final TextEditingController firstNameController = TextEditingController(
-    text: "Test",
-  );
-  final TextEditingController lastNameController = TextEditingController(
-    text: "Mobile",
-  );
-  final TextEditingController emailController = TextEditingController(
-    text: "mobile@example.com",
-  );
-  final TextEditingController phoneController = TextEditingController(
-    text: "1010101010",
-  );
-  final TextEditingController passwordController = TextEditingController(
-    text: "Mobile@123",
-  );
+
+  final TextEditingController firstNameController = TextEditingController();
+  final TextEditingController lastNameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  // final TextEditingController firstNameController = TextEditingController(
+  //   text: "Test",
+  // );
+  // final TextEditingController lastNameController = TextEditingController(
+  //   text: "Mobile",
+  // );
+  // final TextEditingController emailController = TextEditingController(
+  //   text: "mobile@example.com",
+  // );
+  // final TextEditingController phoneController = TextEditingController(
+  //   text: "1010101010",
+  // );
+  // final TextEditingController passwordController = TextEditingController(
+  //   text: "Mobile@123",
+  // );
 
   final ValueNotifier<bool> obscurePassword = ValueNotifier(true);
   final ValueNotifier<bool> agreedToTerms = ValueNotifier(false);
