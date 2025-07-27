7b1169592765237f409797eed2dee8b8ccf576bb
Rahul Chaudhari <chaudhariraul5@gmail.com>
2025-07-28 03:29:55 +0545

    add flag to skip auth interceptor while password reset

--- lib/core/network/auth_interceptor.dart
+++ lib/core/network/auth_interceptor.dart
@@ -7,6 +7,12 @@ class AuthInterceptor extends Interceptor {
     RequestOptions options,
     RequestInterceptorHandler handler,
   ) async {
+    // added this condition to check for our custom flag
+    // if true just pass request along without adding login token
+    if (options.extra['skipAuthInterceptor'] == true) {
+      return handler.next(options);
+    }
+
     final prefs = await SharedPreferences.getInstance();
     final token = prefs.getString("token");
 
