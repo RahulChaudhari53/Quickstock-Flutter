c13f35d96b8115246f9ee7f48b2cc65148989069
Rahul Chaudhari <chaudhariraul5@gmail.com>
2025-07-28 03:40:00 +0545

    initialize password reset in service locator

--- lib/app/service_locator/service_locator.dart
+++ lib/app/service_locator/service_locator.dart
@@ -3,6 +3,16 @@ import 'package:get_it/get_it.dart';
 import 'package:quickstock/app/shared_pref/token_shared_pref.dart';
 import 'package:quickstock/core/network/api_service.dart';
 import 'package:quickstock/core/network/hive_service.dart';
+import 'package:quickstock/features/forgot_password/data/data_source/forgot_password_data_source.dart';
+import 'package:quickstock/features/forgot_password/data/data_source/remote_data_source/forgot_password_remote_data_source.dart';
+import 'package:quickstock/features/forgot_password/data/repository/remote_repository/forgot_password_remote_repository.dart';
+import 'package:quickstock/features/forgot_password/domain/repository/forgot_password_repository.dart';
+import 'package:quickstock/features/forgot_password/domain/usecase/reset_password_usecase.dart';
+import 'package:quickstock/features/forgot_password/domain/usecase/send_otp_usecase.dart';
+import 'package:quickstock/features/forgot_password/domain/usecase/verify_otp_usecase.dart';
+import 'package:quickstock/features/forgot_password/presentation/viewmodel/reset_password_viewmodel/reset_password_viewmodel.dart';
+import 'package:quickstock/features/forgot_password/presentation/viewmodel/send_otp_viewmodel/send_otp_view_model.dart';
+import 'package:quickstock/features/forgot_password/presentation/viewmodel/verify_otp_viewmodel/verify_otp_view_model.dart';
 import 'package:quickstock/features/home/presentation/view_model/home_view_model.dart';
 import 'package:quickstock/features/splash/presentation/view_model/splash_view_model.dart';
 import 'package:quickstock/features/auth/data/data_source/local_data_source/user_local_data_source.dart';
@@ -23,6 +33,7 @@ Future<void> initDependencies() async {
   await _initSharedPref();
 
   await _initSplashModule();
+  await _initForgotPasswordModule();
   await _initUserModule();
   await _initDashboardModule();
 }
@@ -101,6 +112,48 @@ Future<void> _initUserModule() async {
 
   serviceLocator.registerFactory(
     () => LoginViewModel(serviceLocator<UserLoginUsecase>()),
+  );
+}
+
+Future<void> _initForgotPasswordModule() async {
+  // ============== Data Source ==============
+  serviceLocator.registerFactory<ForgotPasswordRemoteDataSource>(
+    () => ForgotPasswordRemoteDataSource(
+      apiService: serviceLocator<ApiService>(),
+    ),
+  );
+
+  // ============== Repository ==============
+  serviceLocator.registerFactory<IForgotPasswordRepository>(
+    () => ForgotPasswordRemoteRepository(
+      forgotPasswordRemoteDataSource:
+          serviceLocator<ForgotPasswordRemoteDataSource>(),
+    ),
+  );
+
+  // ============== UseCases ==============
+  serviceLocator.registerFactory(
+    () => SendOtpUseCase(serviceLocator<IForgotPasswordRepository>()),
+  );
+  serviceLocator.registerFactory(
+    () => VerifyOtpUseCase(serviceLocator<IForgotPasswordRepository>()),
+  );
+  serviceLocator.registerFactory(
+    () => ResetPasswordUseCase(serviceLocator<IForgotPasswordRepository>()),
+  );
+
+  // ============== ViewModels (BLoCs) ==============
+  serviceLocator.registerFactory(
+    () => SendOtpViewModel(serviceLocator<SendOtpUseCase>()),
+  );
+  serviceLocator.registerFactory(
+    () => VerifyOtpViewModel(
+      serviceLocator<VerifyOtpUseCase>(),
+      serviceLocator<SendOtpUseCase>(),
+    ),
+  );
+  serviceLocator.registerFactory(
+    () => ResetPasswordViewModel(serviceLocator<ResetPasswordUseCase>()),
   );
 }
 
