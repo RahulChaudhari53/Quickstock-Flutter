0dcce9cab1c727b9d862af78d2b51d0677b6300e
Rahul Chaudhari <chaudhariraul5@gmail.com>
2025-07-28 03:28:31 +0545

    add endpoints for forgot password

--- lib/app/constant/api_endpoints.dart
+++ lib/app/constant/api_endpoints.dart
@@ -7,11 +7,14 @@ class ApiEndpoints {
 
   // Emulator server
   // static const String serverAddress = "http://10.0.2.2:5050";
-  static const String serverAddress = "http://192.168.1.72:5050";
+  static const String serverAddress = "http://192.168.1.70:5050";
 
   static const String baseUrl = "$serverAddress/api/users/";
 
   // Auth
   static const String login = "login";
   static const String register = "signup";
+  static const String sendOtp = "forgotPassword";
+  static const String verifyOtp = "verify-otp";
+  static const String resetPassword = "resetPassword";
 }
