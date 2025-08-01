6d79f5643c13c69d78084271358bd13c3cf6cd3c
Rahul Chaudhari <chaudhariraul5@gmail.com>
2025-08-01 14:06:12 +0545

    add category entities

---
+++ lib/features/categories/domain/entity/category_entity.dart
@@ -0,0 +1,23 @@
+// lib/features/categories/domain/entities/category_entity.dart
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+
+@immutable
+class CategoryEntity extends Equatable {
+  final String id;
+  final String name;
+  final String description;
+  final String createdBy;
+  final bool isActive;
+
+  const CategoryEntity({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.createdBy,
+    required this.isActive,
+  });
+
+  @override
+  List<Object?> get props => [id, name, description, createdBy, isActive];
+}
---
+++ lib/features/categories/domain/entity/pagination_info_entity.dart
@@ -0,0 +1,27 @@
+// lib/features/categories/domain/entities/pagination_info_entity.dart
+
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class PaginationInfoEntity extends Equatable {
+  final int currentPage;
+  final int totalPages;
+  final bool hasNextPage;
+  final bool hasPrevPage;
+
+  const PaginationInfoEntity({
+    required this.currentPage,
+    required this.totalPages,
+    required this.hasNextPage,
+    required this.hasPrevPage,
+  });
+
+  @override
+  List<Object?> get props => [
+    currentPage,
+    totalPages,
+    hasNextPage,
+    hasPrevPage,
+  ];
+}
