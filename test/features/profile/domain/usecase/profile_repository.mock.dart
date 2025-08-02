46919a30292fb11ea69a6fd94222272d73d94543
Rahul Chaudhari <chaudhariraul5@gmail.com>
2025-08-02 17:57:54 +0545

    add mock profile repository

---
+++ test/features/profile/domain/usecase/profile_repository.mock.dart
@@ -0,0 +1,4 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:quickstock/features/profile/domain/repository/profile_repository.dart';
+
+class MockProfileRepository extends Mock implements IProfileRepository {}
